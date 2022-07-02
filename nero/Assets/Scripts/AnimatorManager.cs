e6f164e6ed8a49b928af8c9088e8c14908b6738e
headlesscrown <mdh8533@nyu.edu>
2022-07-02 19:37:12 -0400

    snapped anim update

---
+++ nero/Assets/Scripts/AnimatorManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour
+{
+    Animator animator;
+    int horizontal;
+    int vertical;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        horizontal = Animator.StringToHash("Horizontal"); //ref horizontal anim values
+        vertical = Animator.StringToHash("Vertical"); //ref vertical anim values
+    }
+
+    public void UpdateAnimatorValues(float horizontalMovement, float verticalMovement)
+    {
+        //animation snapping; round values
+        float snappedHorizontal;
+        float snappedVertical;
+
+        #region //snappedHoriMovt
+        if (horizontalMovement > 0 && horizontalMovement < 0.55f)
+        {
+            snappedHorizontal = 0.5f;
+        }
+        else if(horizontalMovement > 0.55f)
+        {
+            snappedHorizontal = 1;
+        }
+        else if(horizontalMovement < 0 && horizontalMovement > 0.55f)
+        {
+            snappedHorizontal = -0.5f;
+        }
+        else if(horizontalMovement < -0.55f)
+        {
+            snappedHorizontal = -1;
+        }
+        else
+        {
+            snappedHorizontal = 0;
+        }
+        #endregion
+
+        #region //snappedVertMovt
+        if (verticalMovement > 0 && verticalMovement < 0.55f)
+        {
+            snappedVertical = 0.5f;
+        }
+        else if (verticalMovement > 0.55f)
+        {
+            snappedVertical = 1;
+        }
+        else if (verticalMovement < 0 && verticalMovement > 0.55f)
+        {
+            snappedVertical = -0.5f;
+        }
+        else if (verticalMovement < -0.55f)
+        {
+            snappedVertical = -1;
+        }
+        else
+        {
+            snappedVertical = 0;
+        }
+        #endregion
+
+        animator.SetFloat(horizontal, horizontalMovement, 0.1f, Time.deltaTime); //set damp time (blend time) to 0.1f
+        animator.SetFloat(vertical, verticalMovement, 0.1f, Time.deltaTime);
+    }
+}
