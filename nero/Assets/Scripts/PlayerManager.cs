905d0af30bfdb1a18ec7bb836b637d2b6ea99585
headlesscrown <mdh8533@nyu.edu>
2022-07-03 16:12:21 -0400

    update

---
+++ nero/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nero
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        InputManager inputManager;
+        PlayerLocomotion playerLocomotion;
+
+        public void Awake()
+        {
+            inputManager = GetComponent<InputManager>();
+            playerLocomotion = GetComponent<PlayerLocomotion>(); //check for object with locomotion comp
+
+            Cursor.visible = false;
+        }
+
+        private void Update()
+        {
+            inputManager.HandleAllInput();
+        }
+
+        private void FixedUpdate()
+        {
+            playerLocomotion.HandleAllMovement();
+        }
+    }
+}
