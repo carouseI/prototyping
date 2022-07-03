5bd81ecfcd109ae96d10c82878c95a572e8eb9f6
headlesscrown <mdh8533@nyu.edu>
2022-07-03 15:12:42 -0400

    locomotion script update

--- nero/Assets/Scripts/PlayerLocomotion.cs
+++ nero/Assets/Scripts/PlayerLocomotion.cs
@@ -2,71 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerLocomotion : MonoBehaviour
+namespace Nero
 {
-    InputManager inputManager;
-
-    Vector3 moveDirection;
-    Transform cameraObject;
-    Rigidbody playerRigidbody;
-
-    [HideInInspector]
-    public Transform myTransform;
-    [HideInInspector]
-    public AnimatorManager animatorManager;
-
-    [SerializeField]
-    public float movementSpeed = 7;
-    [SerializeField]
-    public float rotationSpeed = 15;
-
-    void Start()
+    public class PlayerLocomotion : MonoBehaviour
     {
-        inputManager = GetComponent<InputManager>();
-        playerRigidbody = GetComponent<Rigidbody>();
-        animatorManager = GetComponent<AnimatorManager>();
-        cameraObject = Camera.main.transform; //scan scene for default camera tag
-        myTransform = transform;
-    }
+        InputManager inputManager;
 
-    public void Update()
-    {
-        float delta = Time.deltaTime;
-    }
+        Vector3 moveDirection;
+        Transform cameraObject;
+        Rigidbody playerRigidbody;
 
-    public void HandleAllMovement()
-    {
-        HandleMovement();
-        HandleRotation();
-    }
+        [HideInInspector]
+        public Transform myTransform;
+        [HideInInspector]
+        public AnimatorManager animatorManager;
 
-    private void HandleMovement()
-    {
-        moveDirection = cameraObject.forward * inputManager.verticalInput; //set camera to track player movement on vertical axis
-        moveDirection = moveDirection + cameraObject.right * inputManager.horizontalInput; //track horizontal movement
-        moveDirection.Normalize(); //maintain direction vector + set length to 1
-        moveDirection.y = 0; //lock player's vertical movement; ground player
-        moveDirection = moveDirection * movementSpeed;
+        [SerializeField]
+        public float movementSpeed = 7;
+        [SerializeField]
+        public float rotationSpeed = 15;
 
-        Vector3 movementVelocity = moveDirection;
-        playerRigidbody.velocity = movementVelocity; //move player according to calculations
-    }
+        public void Awake()
+        {
+            inputManager = GetComponent<InputManager>();
+            playerRigidbody = GetComponent<Rigidbody>();
+            cameraObject = Camera.main.transform; //scan scene for default camera tag
+        }
 
-    private void HandleRotation()
-    {
-        Vector3 targetDirection = Vector3.zero; //default value = 0
+        public void HandleAllMovement()
+        {
+            HandleMovement();
+            HandleRotation();
+        }
 
-        targetDirection = cameraObject.forward * inputManager.verticalInput;
-        targetDirection = targetDirection + cameraObject.right * inputManager.horizontalInput;
-        targetDirection.Normalize();
-        targetDirection.y = 0;
+        private void HandleMovement()
+        {
+            moveDirection = cameraObject.forward * inputManager.verticalInput; //set camera to track player movement on vertical axis
+            moveDirection = moveDirection + cameraObject.right * inputManager.horizontalInput; //track horizontal movement
+            moveDirection.Normalize(); //maintain direction vector + set length to 1
+            moveDirection.y = 0; //lock player's vertical movement; ground player
+            moveDirection = moveDirection * movementSpeed;
 
-        if (targetDirection == Vector3.zero)
-            targetDirection = transform.forward; //set rotation to position when player stops movement
+            Vector3 movementVelocity = moveDirection;
+            playerRigidbody.velocity = movementVelocity; //move player according to calculations
+        }
 
-        Quaternion targetRotation = Quaternion.LookRotation(targetDirection); //quaternion = unity rotation calculations
-        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime); //slerp = rotation between point A + B
+        private void HandleRotation()
+        {
+            Vector3 targetDirection = Vector3.zero; //default value = 0
 
-        transform.rotation = playerRotation;
+            targetDirection = cameraObject.forward * inputManager.verticalInput;
+            targetDirection = targetDirection + cameraObject.right * inputManager.horizontalInput;
+            targetDirection.Normalize();
+            targetDirection.y = 0;
+
+            if (targetDirection == Vector3.zero)
+                targetDirection = transform.forward; //set rotation to position when player stops movement
+
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection); //quaternion = unity rotation calculations
+            Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime); //slerp = rotation between point A + B
+
+            transform.rotation = playerRotation;
+        }
     }
 }
