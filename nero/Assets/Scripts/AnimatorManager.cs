4ce62b9ee10f97f94ab29f67b27ba28c3cda59b7
headlesscrown <mdh8533@nyu.edu>
2022-07-03 15:41:17 -0400

    script update

--- nero/Assets/Scripts/AnimatorManager.cs
+++ nero/Assets/Scripts/AnimatorManager.cs
@@ -6,88 +6,18 @@ namespace Nero
 {
     public class AnimatorManager : MonoBehaviour
     {
-        PlayerManager playerManager;
-        PlayerLocomotion playerLocomotion;
-
         public Animator animator;
+        public Animator anim;
 
         int vertical;
         int horizontal;
 
-        public bool canRotate;
-
-        public void Initialize()
+        private void Awake()
         {
-            playerManager = GetComponent<PlayerManager>();
             animator = GetComponent<Animator>();
 
             vertical = Animator.StringToHash("Vertical");
             horizontal = Animator.StringToHash("Horizontal");
-        }
-
-        public void UpdateAnimatorValues(float verticalMovement, float horizontalMovement, bool isWalking)
-        {
-            float snappedVertical;
-            float snappedHorizontal;
-
-            #region snapped vertical
-            if (verticalMovement > 0 && verticalMovement < 0.55f)
-            {
-                snappedVertical = 0.5f;
-            }
-            else if (verticalMovement > 0.55f)
-            {
-                snappedVertical = 1;
-            }
-            else if (verticalMovement < 0 && verticalMovement > 0.55f)
-            {
-                snappedVertical = -0.5f;
-            }
-            else if (verticalMovement < -0.55f)
-            {
-                snappedVertical = -1;
-            }
-            else
-            {
-                snappedVertical = 0;
-            }
-            #endregion
-            #region snapped horizontal
-            if (horizontalMovement > 0 && horizontalMovement < 0.55f)
-            {
-                snappedHorizontal = 0.5f;
-            }
-            else if (horizontalMovement > 0.55f)
-            {
-                snappedHorizontal = 1;
-            }
-            else if (horizontalMovement < 0 && horizontalMovement > 0.55f)
-            {
-                snappedHorizontal = -0.5f;
-            }
-            else if (horizontalMovement < -0.55f)
-            {
-                snappedHorizontal = -1;
-            }
-            else
-            {
-                snappedHorizontal = 0;
-            }
-            #endregion
-
-            animator.SetFloat(vertical, snappedVertical, 0.1f, Time.deltaTime);
-            animator.SetFloat(horizontal, snappedHorizontal, 0.1f, Time.deltaTime);
-
-        }
-
-        public void CanRotate()
-        {
-            canRotate = true;
-        }
-
-        public void StopRotation()
-        {
-            canRotate = false;
         }
     }
 }
