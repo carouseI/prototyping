052eb23a857ca64c5fa4f32fa6b8591141a7442a
headlesscrown <mdh8533@nyu.edu>
2022-07-03 15:17:40 -0400

    input manager update

--- nero/Assets/Scripts/InputManager.cs
+++ nero/Assets/Scripts/InputManager.cs
@@ -2,49 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManager : MonoBehaviour
+namespace Nero
 {
-    PlayerControls playerControls;
-    AnimatorManager animatorManager;
-
-    public Vector2 movementInput; //variables set on 2 axes -- left + right; up + down
-    private float moveAmount;
-    public float verticalInput;
-    public float horizontalInput;
-
-    private void Awake()
+    public class InputManager : MonoBehaviour
     {
-        animatorManager = GetComponent<AnimatorManager>();
-    }
+        PlayerControls playerControls;
+        AnimatorManager animatorManager;
 
-    private void OnEnable()
-    {
-        if (playerControls == null) //if not filled in
+        public Vector2 movementInput; //variables set on 2 axes -- left + right; up + down
+        public Vector2 cameraInput;
+
+        public float cameraInputX;
+        public float cameraInputY;
+
+        private float moveAmount;
+        public float verticalInput;
+        public float horizontalInput;
+
+        private void Awake()
         {
-            playerControls = new PlayerControls(); //set variable to new control setup
-
-            playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>(); //record movement to vector2 variable
+            animatorManager = GetComponent<AnimatorManager>();
         }
 
-        playerControls.Enable();
-    }
+        private void OnEnable()
+        {
+            if (playerControls == null) //if not filled in
+            {
+                playerControls = new PlayerControls(); //set variable to new control setup
 
-    private void OnDisable() //if attached object is disabled
-    {
-        playerControls.Disable(); //turn off player controls
-    }
+                playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>(); //record movement to vector2 variable
+            }
 
+            playerControls.Enable();
+        }
 
-    public void HandleAllInputs()
-    {
-        HandleMovementInput();
-    }
+        private void OnDisable() //if attached object is disabled
+        {
+            playerControls.Disable(); //turn off player controls
+        }
 
-    private void HandleMovementInput()
-    {
-        verticalInput = movementInput.y;
-        horizontalInput = movementInput.x;
-        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput)); //clamp value between 0 + 1; abs = set to positive
-        animatorManager.UpdateAnimatorValues(0, moveAmount);
+        public void HandleAllInputs()
+        {
+            HandleMovementInput();
+        }
+
+        private void HandleMovementInput()
+        {
+            verticalInput = movementInput.y;
+            horizontalInput = movementInput.x;
+
+            cameraInputY = cameraInput.y;
+            cameraInputX = cameraInput.x;
+
+            moveAmount = Mathf.Clamp01(Mathf.Abs(verticalInput) + Mathf.Abs(horizontalInput)); //clamp value between 0 + 1; abs = set to positive
+            animatorManager.UpdateAnimatorValues(0, moveAmount);
+        }
     }
 }
