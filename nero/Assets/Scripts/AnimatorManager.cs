ac277d3c95ff55ffdaa88e3e850f9fefc2e909f8
headlesscrown <mdh8533@nyu.edu>
2022-07-03 15:07:14 -0400

    anim handler + manager script swap test

--- nero/Assets/Scripts/AnimatorManager.cs
+++ nero/Assets/Scripts/AnimatorManager.cs
@@ -2,82 +2,92 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimatorManager : MonoBehaviour
+namespace Nero
 {
-    Animator animator;
-    int horizontal;
-    int vertical;
-    public bool canRotate;
-
-    private void Awake()
+    public class AnimatorManager : MonoBehaviour
     {
-        animator = GetComponent<Animator>();
-        horizontal = Animator.StringToHash("Horizontal"); //ref horizontal anim values
-        vertical = Animator.StringToHash("Vertical"); //ref vertical anim values
-    }
+        PlayerManager playerManager;
+        PlayerLocomotion playerLocomotion;
 
-    public void UpdateAnimatorValues(float horizontalMovement, float verticalMovement)
-    {
-        //animation snapping; round values
-        float snappedHorizontal;
-        float snappedVertical;
+        public Animator animator;
 
-        #region snapped horizontal
-        if (horizontalMovement > 0 && horizontalMovement < 0.55f)
-        {
-            snappedHorizontal = 0.5f;
-        }
-        else if(horizontalMovement > 0.55f)
-        {
-            snappedHorizontal = 1;
-        }
-        else if(horizontalMovement < 0 && horizontalMovement > 0.55f)
-        {
-            snappedHorizontal = -0.5f;
-        }
-        else if(horizontalMovement < -0.55f)
-        {
-            snappedHorizontal = -1;
-        }
-        else
-        {
-            snappedHorizontal = 0;
-        }
-        #endregion
-        #region snapped vertical
-        if (verticalMovement > 0 && verticalMovement < 0.55f)
-        {
-            snappedVertical = 0.5f;
-        }
-        else if (verticalMovement > 0.55f)
-        {
-            snappedVertical = 1;
-        }
-        else if (verticalMovement < 0 && verticalMovement > 0.55f)
-        {
-            snappedVertical = -0.5f;
-        }
-        else if (verticalMovement < -0.55f)
-        {
-            snappedVertical = -1;
-        }
-        else
-        {
-            snappedVertical = 0;
-        }
-        #endregion
+        int vertical;
+        int horizontal;
 
-        animator.SetFloat(horizontal, snappedHorizontal, 0.1f, Time.deltaTime); //set damp time (blend time) to 0.1f
-        animator.SetFloat(vertical, snappedVertical, 0.1f, Time.deltaTime);
-    }
+        public bool canRotate;
 
-    public void CanRotate()
-    {
-        canRotate = true;
-    }
+        public void Initialize()
+        {
+            playerManager = GetComponent<PlayerManager>();
+            animator = GetComponent<Animator>();
 
-    public void StopRotation()
-    {
-        canRotate = false;
+            vertical = Animator.StringToHash("Vertical");
+            horizontal = Animator.StringToHash("Horizontal");
+        }
+
+        public void UpdateAnimatorValues(float verticalMovement, float horizontalMovement, bool isWalking)
+        {
+            float snappedVertical;
+            float snappedHorizontal;
+
+            #region snapped vertical
+            if (verticalMovement > 0 && verticalMovement < 0.55f)
+            {
+                snappedVertical = 0.5f;
+            }
+            else if (verticalMovement > 0.55f)
+            {
+                snappedVertical = 1;
+            }
+            else if (verticalMovement < 0 && verticalMovement > 0.55f)
+            {
+                snappedVertical = -0.5f;
+            }
+            else if (verticalMovement < -0.55f)
+            {
+                snappedVertical = -1;
+            }
+            else
+            {
+                snappedVertical = 0;
+            }
+            #endregion
+            #region snapped horizontal
+            if (horizontalMovement > 0 && horizontalMovement < 0.55f)
+            {
+                snappedHorizontal = 0.5f;
+            }
+            else if (horizontalMovement > 0.55f)
+            {
+                snappedHorizontal = 1;
+            }
+            else if (horizontalMovement < 0 && horizontalMovement > 0.55f)
+            {
+                snappedHorizontal = -0.5f;
+            }
+            else if (horizontalMovement < -0.55f)
+            {
+                snappedHorizontal = -1;
+            }
+            else
+            {
+                snappedHorizontal = 0;
+            }
+            #endregion
+
+            animator.SetFloat(vertical, snappedVertical, 0.1f, Time.deltaTime);
+            animator.SetFloat(horizontal, snappedHorizontal, 0.1f, Time.deltaTime);
+
+        }
+
+        public void CanRotate()
+        {
+            canRotate = true;
+        }
+
+        public void StopRotation()
+        {
+            canRotate = false;
+        }
     }
 }
