b4145a1b997116d68617cb3b578fb323e273479c
headlesscrown <mdh8533@nyu.edu>
2022-07-16 14:44:45 -0400

    script cleanup

--- nero/Assets/Scripts/PlayerManager.cs
+++ nero/Assets/Scripts/PlayerManager.cs
@@ -9,30 +9,6 @@ namespace Nero
         PlayerCamera playerCamera;
         InputManager inputManager;
 
-        #region old
-        //InputManager inputManager;
-        //CameraManager cameraManager;
-        //PlayerLocomotion playerLocomotion;
-
-        //public void Awake()
-        //{
-        //    inputManager = GetComponent<InputManager>();
-        //    playerLocomotion = GetComponent<PlayerLocomotion>(); //check for object with locomotion comp
-
-        //    Cursor.visible = false;
-        //}
-
-        //private void Update()
-        //{
-        //    inputManager.HandleAllInput();
-        //}
-
-        //private void FixedUpdate()
-        //{
-        //    playerLocomotion.HandleAllMovement();
-        //}
-        #endregion
-
         private void Awake()
         {
             playerCamera = FindObjectOfType<PlayerCamera>();
