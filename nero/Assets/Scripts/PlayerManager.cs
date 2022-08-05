8c266da24f489f7882e7ab0ce627f89165418758
headlesscrown <mdh8533@nyu.edu>
2022-08-05 16:33:10 -0400

    temp save

--- nero/Assets/Scripts/PlayerManager.cs
+++ nero/Assets/Scripts/PlayerManager.cs
@@ -24,7 +24,7 @@ namespace Nero
 
         private void FixedUpdate()
         {
-            //playerLocomotion.HandleAllMovement();
+            playerLocomotion.HandleAllMovement();
         }
 
         private void LateUpdate()
