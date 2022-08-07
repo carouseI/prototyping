bee76dc15ac6b2575d133e771de6a196cad72fe5
headlesscrown <mdh8533@nyu.edu>
2022-08-06 23:06:44 -0400

    camera rig setup

--- nero/Assets/Scripts/PlayerCamera.cs
+++ nero/Assets/Scripts/PlayerCamera.cs
@@ -4,24 +4,53 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
+    public InputManager inputManager;
+
     public Transform cameraPivot;
     public Camera cameraObject;
     public GameObject player;
 
     Vector3 cameraFollowVelocity = Vector3.zero;
     Vector3 targetPosition;
+    Vector3 cameraRotation;
+    Quaternion targetRotation;
 
     [Header("Camera Speed")]
     public float cameraSmoothTime = 0.2f;
 
+    float lookAmountVertical;
+    float lookAmountHorizontal;
+    float maximumPivotAngle = 15;
+    float minimumPivotAngel = -15;
+
     public void HandleAllCameraMovement()
     {
         FollowPlayer();
+        RotateCamera();
     }
 
     private void FollowPlayer()
     {
         targetPosition = Vector3.SmoothDamp(transform.position, player.transform.position, ref cameraFollowVelocity, cameraSmoothTime * Time.deltaTime); //targetPos @ playerPos + ref from camera
         transform.position = targetPosition;
+    }
+
+    private void RotateCamera()
+    {
+        lookAmountVertical = lookAmountVertical + (inputManager.horizontalCameraInput);
+        lookAmountHorizontal = lookAmountHorizontal - (inputManager.verticalCameraInput);
+        lookAmountHorizontal = Mathf.Clamp(lookAmountHorizontal, minimumPivotAngel, maximumPivotAngle); //lock values between min + max
+
+        cameraRotation = Vector3.zero;
+        cameraRotation.y = lookAmountVertical;
+        targetRotation = Quaternion.Euler(cameraRotation);
+        targetRotation = Quaternion.Slerp(transform.rotation, targetRotation, cameraSmoothTime);
+        transform.rotation = targetRotation;
+
+        cameraRotation = Vector3.zero;
+        cameraRotation.x = lookAmountHorizontal;
+        targetRotation = Quaternion.Euler(cameraRotation);
+        targetRotation = Quaternion.Slerp(cameraPivot.localRotation, targetRotation, cameraSmoothTime); //chang up/down via pivot rotation, left/right via game obj rotation
+        cameraPivot.localRotation = targetRotation;
     }
 }
