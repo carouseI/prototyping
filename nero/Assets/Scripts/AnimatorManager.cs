2e020ecccbb40ad5e9c3134db89743767ec7d459
headlesscrown <mdh8533@nyu.edu>
2022-08-06 20:58:06 -0400

    mass delete

--- nero/Assets/Scripts/AnimatorManager.cs
+++ nero/Assets/Scripts/AnimatorManager.cs
@@ -2,56 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Nero
+public class AnimatorManager : MonoBehaviour
 {
-    public class AnimatorManager : MonoBehaviour
+    Animator animator;
+
+    float snappedHorizontal;
+    float snappedVertical;
+
+    private void Awake()
     {
-        public Animator animator;
-        float snappedVertical;
-        float snappedHorizontal;
+        animator = GetComponent<Animator>();
+    }
 
-        private void Awake()
+    public void HandleAnimatorValues(float horizontalMovement, float verticalMovement)
+    {
+        animator.SetFloat("Horizontal", horizontalMovement, 0.1f, Time.deltaTime);
+        animator.SetFloat("Vertical", verticalMovement, 0.1f, Time.deltaTime);
+
+        #region snapped horizontal
+        if (horizontalMovement > 0) //if greater than 0
         {
-            animator = GetComponent<Animator>();
+            snappedHorizontal = 1; //default to 1
         }
-
-        public void HandleAnimatorValues(float verticalMovement, float horizontalMovement)
+        else if (horizontalMovement < 0) //if less than 0
         {
-            animator.SetFloat("Vertical", verticalMovement, 0.1f, Time.deltaTime);
-            animator.SetFloat("Horizontal", horizontalMovement, 0.1f, Time.deltaTime);
-
-            #region Snapped Vertical
-            if (verticalMovement > 0) //if greater than 0
-            {
-                snappedVertical = 1; //default to 1
-            }
-            else if (verticalMovement < 0) //if greater than 0
-            {
-                snappedVertical = -1; //default to -1
-            }
-            else
-            {
-                snappedVertical = 0; //default to 0
-            }
-            #endregion
-
-            #region snapped horizontal
-            if (horizontalMovement > 0) //if greater than 0
-            {
-                snappedHorizontal = 1; //default to 1
-            }
-            else if (horizontalMovement < 0) //if less than 0
-            {
-                snappedHorizontal = -1; //default to -1
-            }
-            else
-            {
-                snappedHorizontal = 0; //default to 0
-            }
-            #endregion
-
-            animator.SetFloat("Vertical", snappedVertical, 0.1f, Time.deltaTime); //set snapped vert animation; 0.1f = damp/blend time, transition between actions to smooth out motion
-            animator.SetFloat("Horizontal", snappedHorizontal, 0.1f, Time.deltaTime); //set snapped hori animation
+            snappedHorizontal = -1; //default to -1
         }
+        else
+        {
+            snappedHorizontal = 0; //default to 0
+        }
+        #endregion
+
+        #region Snapped Vertical
+        if (verticalMovement > 0) //if greater than 0
+        {
+            snappedVertical = 1; //default to 1
+        }
+        else if (verticalMovement < 0) //if greater than 0
+        {
+            snappedVertical = -1; //default to -1
+        }
+        else
+        {
+            snappedVertical = 0; //default to 0
+        }
+        #endregion
+
+        animator.SetFloat("Horizontal", snappedHorizontal, 0.1f, Time.deltaTime); //set snapped hori animation
+        animator.SetFloat("Vertical", snappedVertical, 0.1f, Time.deltaTime); //set snapped vert animation; 0.1f = damp/blend time, transition between actions to smooth out motion
     }
 }
