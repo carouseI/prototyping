4fc0edb8cabaf51c675a4b3a9b3460c9cb594400
headlesscrown <mdh8533@nyu.edu>
2022-08-06 23:15:22 -0400

    update

---
+++ nero/Assets/Scripts/PlayerLocomotionManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLocomotionManager : MonoBehaviour
+{
+    InputManager inputManager;
+
+    [Header("Camera Transform")]
+    public Transform cameraHolderTransform;
+
+    [Header("Movement Speed")]
+    public float rotationSpeed = 3.5f;
+
+    [Header("Rotation Variables")]
+    Quaternion targetRotation;
+    Quaternion playerRotation;
+
+    private void Awake()
+    {
+        inputManager = GetComponent<InputManager>();
+    }
+
+    public void HandleAllLocomotion()
+    {
+        HandleRotation();
+    }
+
+    private void HandleRotation()
+    {
+        targetRotation = Quaternion.Euler(0, cameraHolderTransform.eulerAngles.y, 0); //only rotate towards 1 direction/where camera is facing
+        playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        if(inputManager.verticalMovementInput != 0 || inputManager.horizontalMovementInput != 0) //only rotate when there is movement input
+        {
+            transform.rotation = playerRotation;
+        }
+    }
+}
--- nero/Assets/Scripts/PlayerManager.cs
+++ nero/Assets/Scripts/PlayerManager.cs
@@ -6,16 +6,23 @@ public class PlayerManager : MonoBehaviour
 {
     PlayerCamera playerCamera;
     InputManager inputManager;
+    PlayerLocomotionManager playerLocomotionManager;
 
     private void Awake()
     {
         playerCamera = FindObjectOfType<PlayerCamera>();
         inputManager = GetComponent<InputManager>();
+        playerLocomotionManager = GetComponent<PlayerLocomotionManager>();
     }
 
     private void Update()
     {
         inputManager.HandleAllInput();
+    }
+
+    private void FixedUpdate()
+    {
+        playerLocomotionManager.HandleAllLocomotion();
     }
 
     private void LateUpdate()
